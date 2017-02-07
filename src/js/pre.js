942b4f1db9bf59b257e69a6e1ced07815d91b9a1
Zero Tung <zero_tung@163.com>
2017-02-08 00:49:23 +0800

    Bug fixed

--- src/js/pre.js
+++ src/js/pre.js
@@ -27,23 +27,26 @@ function isPreloadFinished() {
     }
 }
 
-preload(
-    // start page
-    'static/StartIcon.png',
-    'static/StartBtn.png',
-    // game page
-    'static/Aotuman.png',
-    'static/Key.png',
-    'static/MonsIcon.png',
-    'static/Monsters.png',
-    'static/PowerFill.png',
-    'static/PowerSlot.png',
-    'static/game_bg.jpg',
-    'static/X.png',
-    // end page
-    'static/Grass2.png',
-    'static/Man2.png',
-    'static/ScoreBoard.png',
-    'static/Restart.png',
-    'static/Share.png',
-);
+// 等待加载页的图片加载完后加载其他图片
+window.onload = function() {
+    preload(
+        // start page
+        'static/StartIcon.png',
+        'static/StartBtn.png',
+        // game page
+        'static/Aotuman.png',
+        'static/Key.png',
+        'static/MonsIcon.png',
+        'static/Monsters.png',
+        'static/PowerFill.png',
+        'static/PowerSlot.png',
+        'static/game_bg.jpg',
+        'static/X.png',
+        // end page
+        'static/Grass2.png',
+        'static/Man2.png',
+        'static/ScoreBoard.png',
+        'static/Restart.png',
+        'static/Share.png',
+    );
+}
